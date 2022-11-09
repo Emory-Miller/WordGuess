b776b902bc659bdf3fac6ee9d31f4731bfa36467
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2022-11-09 08:54:59 -0500

    Added some more methods...

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,5 +1,9 @@
 package com.github.zipcodewilmington;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 
 /**
  * @author xt0fer
@@ -7,4 +11,62 @@ package com.github.zipcodewilmington;
  * @date 5/27/21 11:02 AM
  */
 public class Hangman {
+
+    String[] wordArray = { "cat", "dog", "bog", "cut"};
+    String currentWord = "";
+
+    int cGI = 0; // Current Guesses Integer
+    ArrayList<String> cGA = new ArrayList<>(); // Current Guesses Array
+    ArrayList<String> aWA = new ArrayList<>(); // Actual Word Array
+
+    public String getRandomWord (){
+        Random rand = new Random();
+        int randIndex = rand.nextInt(wordArray.length-1);
+        currentWord = wordArray[randIndex];
+        return currentWord;
+    }
+
+    public String display (String display){
+        System.out.println(display.toString());
+        return display;
+    }
+
+
+    public ArrayList<String> wordToArray(){
+        String[] aWS = currentWord.split("");
+        aWA = (ArrayList<String>) Arrays.asList(aWS);
+        return aWA;
+    }
+
+    public ArrayList<String> wordToGuessArray(){
+
+    }
+
+    public boolean checkGuess(){
+
+    }
+
+    public int countGuesses(){
+        return 0;
+    }
+    public void winningGame(){
+        currentWord = "";
+        aWA.clear();
+        cGA.clear();
+        System.out.println("Congratulations, You Won!");
+        Console.getStringInput("Would you like to play again? (yes/no)");
+    }
+
+    public void losingGame(){
+        currentWord = "";
+        aWA.clear();
+        cGA.clear();
+        System.out.println("You lost! You ran out of guesses.");
+        Console.getStringInput("Would you like to play again? (yes/no)");
+    }
+    public void exitGame(){
+
+    }
+
+
 }
--- src/test/java/com/github/zipcodewilmington/HangmanTest.java
+++ src/test/java/com/github/zipcodewilmington/HangmanTest.java
@@ -1,9 +1,27 @@
 package com.github.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import com.github.zipcodewilmington.Hangman;
+
 /**
  * @author xt0fer
  * @version 1.0.0
  * @date 5/27/21 11:02 AM
  */
 public class HangmanTest {
+
+    @Test
+    public void getRandomWordTest(){
+        Hangman hm = new Hangman();
+        System.out.println(hm.getRandomWord());
+    }
+
+    @Test
+    public void displayTest(){
+        Hangman hm = new Hangman();
+        String expected = "Hello World";
+        String actual = hm.display(expected);
+        Assert.assertEquals(expected, actual);
+    }
 }
